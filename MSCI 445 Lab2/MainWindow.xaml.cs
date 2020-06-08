98fa65965bc2e4786518d14a338915aaafd078aa
b46xu <b46xu@users.noreply.github.com>
2020-06-08 15:56:48 -0400

    Initial commit

---
+++ MSCI 445 Lab2/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MSCI_445_Lab2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		
+		private void ButtonClick(object sender, RoutedEventArgs e)
+		{
+
+			Password_Input password_Input = new Password_Input();
+			this.Visibility = Visibility.Hidden;
+			password_Input.Show();
+			
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+		}
+
+		
+
+		public static void Send_Email(String password1) {
+
+		//get from and to here 
+
+			
+			var smtpClient = new SmtpClient("smtp.gmail.com")
+			{
+				Port = 587,
+				Credentials = new System.Net.NetworkCredential("xbht0412@gmail.com", password1),
+				EnableSsl = true,
+			};
+
+			smtpClient.Send("xbht0412@gmail.com", "xbht0412@gmail.com", "testing", "body");
+		
+
+	}
+	}
+
+
+}
---
+++ MSCI 445 Lab2/Password_Input.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net.Mail;
+
+namespace MSCI_445_Lab2
+{
+    /// <summary>
+    /// Interaction logic for Password_Input.xaml
+    /// </summary>
+    public partial class Password_Input : Window
+    {
+        public Password_Input()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PasswordBox password1Box = (PasswordBox)this.FindName("password1");
+            String password1 = password1Box.Password;
+            MainWindow.Send_Email(password1);
+        }
+    }
+}
